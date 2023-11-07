ec7ece82a86da46a22ea8e5f9e12adac18bb6910
zanetamak <zaneta.zawislak@gmail.com>
2023-11-07 16:14:21 +0100

    part 2

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -12,8 +12,8 @@ export const Statistics = ({ title, stats }) => {
       <ul className={css.statList}>
         {stats.map(stat => (
              <li
-                style={{ backgroundColor: getRandomHexColor() }}
-                // key={stat.id}
+            style={{ backgroundColor: getRandomHexColor() }}
+            key={stat.id}
             className={css.item}
           >
             <span className={css.label}>{stat.label}</span>
@@ -26,8 +26,18 @@ export const Statistics = ({ title, stats }) => {
 };
 
 Statistics.propTypes = {
-    title: PropTypes.string.isRequired,
-}
+    title: PropTypes.string,
+    stats: PropTypes.arrayOf( 
+    // PropTypes.arrayOf oczekuje jednego argumentu, który jest validator'em dla każdego elementu 
+        //w tablicy.
+        PropTypes.shape({
+    // dlatego mozna użyć PropTypes.shape, aby zdefiniować oczekiwany kształt (strukturę) każdego elementu w tablicy.
+            label: PropTypes.string.isRequired,
+            id: PropTypes.string.isRequired,
+            percentage: PropTypes.number.isRequired,
+        })
+    ).isRequired,
+};
 
 export default Statistics;
 
